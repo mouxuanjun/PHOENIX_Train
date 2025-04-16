6b398a3b7f6a240fb2801328efe1450ee0772951
SWSWSWSWH <3357805889@qq.com>
2025-04-16 15:35:04 +0800

    dr16

---
+++ dr16/User/LED.c
@@ -0,0 +1,30 @@
+#include "LED.h"
+#include "main.h"
+#include "cmsis_os.h"
+extern rc_info_t rc;
+
+void LEDTask(void const * argument)
+{
+	while(1)
+  {
+		if(rc.s1 == 1)
+		{
+			HAL_GPIO_WritePin(GPIOH,GPIO_PIN_10,GPIO_PIN_SET);
+			HAL_GPIO_WritePin(GPIOH,GPIO_PIN_11,GPIO_PIN_RESET);
+			HAL_GPIO_WritePin(GPIOH,GPIO_PIN_12,GPIO_PIN_RESET);
+		}
+		if(rc.s1 == 2)
+		{
+			HAL_GPIO_WritePin(GPIOH,GPIO_PIN_11,GPIO_PIN_SET);
+			HAL_GPIO_WritePin(GPIOH,GPIO_PIN_10,GPIO_PIN_RESET);
+			HAL_GPIO_WritePin(GPIOH,GPIO_PIN_12,GPIO_PIN_RESET);
+		}
+		if(rc.s1 == 3)
+		{
+			HAL_GPIO_WritePin(GPIOH,GPIO_PIN_12,GPIO_PIN_SET);
+			HAL_GPIO_WritePin(GPIOH,GPIO_PIN_11,GPIO_PIN_RESET);
+			HAL_GPIO_WritePin(GPIOH,GPIO_PIN_10,GPIO_PIN_RESET);
+		}
+		osDelay(2);
+	}
+}
---
+++ dr16/User/dr16.c
@@ -0,0 +1,14 @@
+#include "dr16.h"
+
+rc_info_t rc;
+
+void Remotedata(rc_info_t *rc,uint8_t tx_data[18])
+{
+	
+	rc->ch0 = (((int16_t) tx_data[0] | (int16_t) tx_data[1] << 8) & 0x07FF) - 1024;
+	rc->ch1 = (((int16_t) tx_data[1] >> 3 | (int16_t) tx_data[2] << 5) & 0x07FF) - 1024;
+	rc->ch2 = (((int16_t) tx_data[2] >> 6 | (int16_t) tx_data[3] | (int16_t) tx_data[4] << 10) & 0x07FF) - 1024;
+	rc->ch3 = (((int16_t) tx_data[4] >> 1 | (int16_t) tx_data[5] << 7) & 0x07FF) - 1024;
+	rc->s1  = ((int16_t) tx_data[5] >> 4) & 0x003;
+	rc->s2  = ((int16_t) tx_data[5] >> 6) & 0x003;
+}
