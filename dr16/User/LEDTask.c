882cf69e5fb4b474e4cd107573a399fd813c7fba
金明轩 <3531870878@qq.com>
2025-04-20 22:14:00 +0800

    ✨ feat: DR16

---
+++ dr16/User/LEDTask.c
@@ -0,0 +1,30 @@
+#include "LEDTask.h"
+#include "main.h"
+#include "dr16.h"
+
+extern rc_info_t rc;
+
+void LEDTask(void const * argument)
+{
+	while(1)
+	{
+		if(rc.s2 == 1)
+		{
+			HAL_GPIO_WritePin(GPIOH,GPIO_PIN_10,GPIO_PIN_SET);
+			HAL_GPIO_WritePin(GPIOH,GPIO_PIN_11,GPIO_PIN_RESET);
+			HAL_GPIO_WritePin(GPIOH,GPIO_PIN_12,GPIO_PIN_RESET);
+		}
+		if(rc.s2 == 2)
+		{
+			HAL_GPIO_WritePin(GPIOH,GPIO_PIN_10,GPIO_PIN_RESET);
+			HAL_GPIO_WritePin(GPIOH,GPIO_PIN_11,GPIO_PIN_SET);
+			HAL_GPIO_WritePin(GPIOH,GPIO_PIN_12,GPIO_PIN_RESET);
+		}
+		if(rc.s2 == 3)
+		{
+			HAL_GPIO_WritePin(GPIOH,GPIO_PIN_10,GPIO_PIN_RESET);
+			HAL_GPIO_WritePin(GPIOH,GPIO_PIN_11,GPIO_PIN_RESET);
+			HAL_GPIO_WritePin(GPIOH,GPIO_PIN_12,GPIO_PIN_SET);
+		}
+	}
+}
---
+++ dr16/User/LEDTask.h
@@ -0,0 +1,10 @@
+#ifndef __LEDTask_H__
+#define __LEDTask_H__
+
+#include "main.h"
+#include "cmsis_os.h"
+#include "freertos.h"
+
+void LEDTask(void const * argument);
+
+#endif
