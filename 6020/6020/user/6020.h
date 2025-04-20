04adde973d915d169774bcfdac7a47b7ebf47632
ZPZ <zpz123zpz@hotmail.com>
2025-04-20 21:29:28 +0800

    🎉 init(电机读数):

---
+++ 6020/6020/user/6020.h
@@ -0,0 +1,13 @@
+#include "main.h"
+
+
+typedef struct {
+    int16_t speed;
+    uint16_t angle;
+    uint8_t temperature;
+    int16_t current;
+}Motor6020;
+
+extern Motor6020 motor6020;
+
+void can_init(void);
